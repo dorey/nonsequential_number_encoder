27cc93491fb2cbfb162ef7fccb5f830012957b94
Alex Dorey <dorey415@gmail.com>
2011-10-20 13:45:17 -0400

    added decoder

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-def nonsequential_encode(num, digits=10, alphabet='abcdefghijklmnopqrstuvwxyz0123456789'):
+def nonsequential_encode(num, digits=10, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
     #Using base36 encode to have a character (or 2) at the end
     # of the xforms id_string that signifies the version number
     # this way, xforms exported on the same day do not have the
@@ -12,6 +12,9 @@ def nonsequential_encode(num, digits=10, alphabet='abcdefghijklmnopqrstuvwxyz012
         base36 = alphabet[i] + base36
     return base36.upper()
 
-print nonsequential_encode(100)
-print nonsequential_encode(101)
-print nonsequential_encode(102)
+def nonsequential_decode(estr):
+    return int(str(int(estr, 36))[::-1])
+
+print nonsequential_decode(nonsequential_encode(100))
+print nonsequential_decode(nonsequential_encode(101))
+print nonsequential_decode(nonsequential_encode(102))
