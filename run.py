53437ade143bbc942530719841fb5b76189d9845
Alex Dorey <dorey415@gmail.com>
2011-10-20 13:34:45 -0400

    nonsequential encoder initial commit

---
+++ run.py
@@ -0,0 +1,13 @@
+def nonsequential_encode(num, digits=10, alphabet='abcdefghijklmnopqrstuvwxyz0123456789'):
+    #Using base36 encode to have a character (or 2) at the end
+    # of the xforms id_string that signifies the version number
+    # this way, xforms exported on the same day do not have the
+    # same ID string.
+    if num == 0:
+        return alphabet[0]
+    num = int(str(num).rjust(digits, '0')[::-1])
+    base36 = ''
+    while num != 0:
+        num, i = divmod(num, len(alphabet))
+        base36 = alphabet[i] + base36
+    return base36.upper()
